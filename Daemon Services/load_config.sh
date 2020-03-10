df6dce631b40adefffdd858a209d125e125b6b65
Murat Bayram <murat.bayram@softwareag.com>
2020-03-10 11:34:13 +0100

    Changed daemon structure

--- Daemon Services/load_config.sh
+++ Daemon Services/load_config.sh
@@ -1,4 +1,4 @@
 #sudo systemctl stop MAC2C8Y.service
-sudo cp MAC2C8Y.service /lib/systemd/system/MAC2C8Y.service
+sudo cp SmartKanban.service /lib/systemd/system/SmartKanban.service
 sudo systemctl daemon-reload
-sudo systemctl start MAC2C8Y.service
+sudo systemctl start SmartKanban.service
